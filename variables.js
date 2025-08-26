27029b09c7f7e9a20c3130d4276160dead8e8e7d
Hafsa web_developer <muhammadnaeed160@gmail.com>
2025-08-26 11:09:19 +0500

    Update variables.js

--- variables.js
+++ variables.js
@@ -3,4 +3,7 @@ let num2 = 12
 let sum = num1 + num2
 console.log(sum)
 let name = "Hafsa"
-comsole.log(`My name is ${name}`)
+console.log(`My name is ${name}`)
+const marks = 90
+console.log (marks)
+
